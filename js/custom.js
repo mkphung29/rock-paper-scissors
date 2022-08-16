a982a86004baacf1fade1d85de2bd40521c25104
Madison P <maddykay396@gmail.com>
2022-08-16 13:23:26 -0400

    Define variables

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,7 @@
+let playerSelection = prompt("Choose Your Weapon!");
+let computerSelection = getComputerChoice();
+
+
 function getComputerChoice{
     let choices = ["Rock", "Paper", "Scissors"];
     let random = choices[Math.floor(Math.random() * choices.length)];
