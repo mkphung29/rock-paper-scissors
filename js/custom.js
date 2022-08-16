8d3ba148898a92bf8788eeb40ce28cd3de76437b
Madison P <maddykay396@gmail.com>
2022-08-16 13:33:33 -0400

    Create conditionals

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,21 @@ function getComputerChoice{
     return random;
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return ("It's a draw");
+    }else if (playerSelection === "Rock") && (computerSelection === "Scissors"){
+        return ("You win! Rock beats scissors.");
+    }else if (playerSelection === "Rock") && (computerSelection === "Paper"){
+        return ("You lose! Paper beats rock.");
+    }else if (playerSelection === "Paper") && (computerSelection === "Rock"){
+        return ("You win! Paper beats rock.");
+    }else if (playerSelection === "Paper") && (computerSelection === "Scissors"){
+        return ("You lose! Scissors beats paper.");
+    }else if (playerSelection === "Scissors") && (computerSelection === "Paper"){
+        return ("You win! Scissors beats paper.");
+    }else if (playerSelection === "Scissors") && (computerSelection === "Rock"){
+        return ("You lose! Rock beats scissors.");
+    }
+
+    }
