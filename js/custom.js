46bd5b26cbad303ceae3760807bcb9c8de342153
Madison P <maddykay396@gmail.com>
2022-08-18 17:47:32 -0400

    Make user input case insensitive

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
 //Define player selection
 let playerSelection = prompt ("Choose your weapon!");
+
 console.log ("You choose " + playerSelection);
+
 
 // Create random computer choice function
 function getComputerChoice(){
@@ -16,26 +18,27 @@ let computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
 
+
 //Create function for one round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
         return ("It's a draw");
-    }else if ((playerSelection === "Rock") && (computerSelection === "Scissors")){
+    }else if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection.toUpperCase() === "SCISSORS")){
         return ("You win! Rock beats scissors");
         playerScore++;
-    }else if ((playerSelection === "Rock") && (computerSelection === "Paper")){
+    }else if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection.toUpperCase() === "PAPER")){
         return ("You lose! Paper beats rock");
         computerScore++;
-    }else if ((playerSelection === "Paper") && (computerSelection === "Rock")){
+    }else if ((playerSelection.toUpperCase() === "PAPER") && (computerSelection.toUpperCase() === "ROCK")){
         return ("You win! Paper beats rock");
         playerScore++;
-    }else if ((playerSelection === "Paper") && (computerSelection === "Scissors")){
+    }else if ((playerSelection.toUpperCase() === "PAPER") && (computerSelection.toUpperCase() === "SCISSORS")){
         return ("You lose! Scissors beats paper");
         computerScore++;
-    }else if ((playerSelection === "Scissors") && (computerSelection === "Paper")){
+    }else if ((playerSelection.toUpperCase() === "SCISSORS") && (computerSelection.toUpperCase() === "PAPER")){
         return ("You win! Scissors beats paper");
         playerScore++;
-    }else if ((playerSelection === "Scissors") && (computerSelection === "Rock")){
+    }else if ((playerSelection.toUpperCase() === "SCISSORS") && (computerSelection.toUpperCase() === "ROCK")){
         return ("You lose! Rock beats scissors");
         computerScore++;
     }
@@ -43,9 +46,6 @@ function playRound(playerSelection, computerSelection) {
 
     //Fix playerScore++ / computerScore++ ???
 
-
-//Define playRound
-console.log(playRound(playerSelection, computerSelection));
 
 
 //Loop
@@ -62,7 +62,7 @@ function game() {
         computerSelection = getComputerChoice();
         console.log ("The computer chooses " + computerSelection);
 
-        console.log (playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection)); //undefined
             }
             if (playerScore > computerScore){
                 return ("You win the game!");
@@ -74,6 +74,8 @@ function game() {
 
 //Prompt 5 rounds:
 console.log(game());
+
+//console.log is undefined
 
     //Fix the results below:
 console.log("Your final score is " +playerScore+ "wins and " +computerScore+ "losses");
